db5c1d437d14731c92b3dbe05c206751a1798ddf
ali <wizard2070@gmail.com>
2023-03-07 20:36:41 +0330

    5th

--- main.php
+++ main.php
@@ -8,7 +8,7 @@
  * Author: ZeroOne
  * Author URI: https://github.com/tuderiewsc
  * Text Domain: prophets_horoscope
- * Domain Path: /l10n
+ * Domain Path: /languages
  */
 defined('ABSPATH') or die('No script kiddies please!');
 define('MYPH_ROOTDIR', plugin_dir_path(__FILE__));
@@ -16,16 +16,19 @@ define('MYPH_INC', MYPH_ROOTDIR . 'includes/');
 define('MYPH_ADMIN', MYPH_ROOTDIR . 'admin/');
 define('MYPH_ADMIN_JS', plugin_dir_url(__FILE__) . 'admin/static/js/');
 define('MYPH_SITE_JS', plugin_dir_url(__FILE__) . 'site/static/js/');
+define('MYPH_SITE_CSS', plugin_dir_url(__FILE__) . 'site/static/css/');
 define('MYPH_HOROSCOPES_TBL', 'horoscopes');
 
 
 add_action('plugins_loaded', function () {
-    load_plugin_textdomain('prophets_horoscope', false, basename(MYPH_ROOTDIR) . '/l10n/');
+    load_plugin_textdomain('prophets_horoscope', false, basename(MYPH_ROOTDIR) . '/languages/');
 });
 
 add_action('admin_enqueue_scripts', function () {
 });
 add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style('myph-main-styles', MYPH_SITE_CSS . 'styles.css', array(), '1.0.0');
+
     wp_enqueue_script('myph-main-script', MYPH_SITE_JS . 'scripts.js', array('jquery'), '1.0.0');
     wp_localize_script('myph-main-script', 'MYPH_SITE_AJAX', array(
         'AJAXURL' => admin_url('admin-ajax.php'),
--- site/templates/horoscopeWidget.php
+++ site/templates/horoscopeWidget.php
@@ -7,15 +7,17 @@
         <figure>
             <img src="<?= plugins_url('static/images/anbia_img.png', dirname(__FILE__)) ?>" alt="<?= __('Prophets Horoscope Picture', 'prophets_horoscope') ?>">
         </figure>
-        <p>
+        <p class="horoscope-content__aye">
             بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ ﴿۱﴾
             الْحَمْدُ للّهِ رَبِّ الْعَالَمِينَ ﴿۲﴾ الرَّحْمنِ الرَّحِيمِ ﴿۳﴾
             مَالِكِ يَوْمِ الدِّينِ ﴿۴﴾ إِيَّاكَ نَعْبُدُ وإِيَّاكَ نَسْتَعِينُ ﴿۵﴾
             اهدِنَا الصِّرَاطَ المُستَقِيمَ ﴿۶﴾
             صِرَاطَ الَّذِينَ أَنعَمتَ عَلَيهِمْ غَيرِ المَغضُوبِ عَلَيهِمْ وَلاَ الضَّالِّينَ ﴿۷﴾
         </p>
-        <p><strong><?= __('niat', 'prophets_horoscope') ?></strong></p>
-        <button onclick="loadRandomHoroscope(event)">Show</button>
-        <input type="hidden" name="horoscope_nonce" id="horoscope_nonce" value="<?= wp_create_nonce('horoscope-nonce') ?>">
+        <p><strong><?= __('Make an intention and click on the button', 'prophets_horoscope') ?></strong></p>
+        <button class="horoscope-content__submitBtn" onclick="loadRandomHoroscope(event)">
+            <?= __('Prophets online horoscope', 'prophets_horoscope') ?>
+        </button>
+        <input type="hidden" id="horoscope_nonce" value="<?= wp_create_nonce('horoscope-nonce') ?>">
     </div>
 </section>
