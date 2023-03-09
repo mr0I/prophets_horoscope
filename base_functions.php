aab2d6bf0e732e8c6ad82380f1406635b0486c0e
ali <wizard2070@gmail.com>
2023-03-09 15:09:45 +0330

    10th

--- base_functions.php
+++ base_functions.php
@@ -3,30 +3,25 @@
 
 function MYPH_activate_function()
 {
-    global $wpdb;
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    require(MYPH_ROOTDIR . 'helpers/db.php');
+    sleep(1);
+    require(MYPH_ROOTDIR . 'helpers/db_seed.php');
 
-    $horoscopes_tbl = $wpdb->prefix . MYPH_HOROSCOPES_TBL;
-    $createHoroscopesTable =
-        "
-		CREATE TABLE IF NOT EXISTS `{$horoscopes_tbl}` (
-            `id` int(11) NOT NULL AUTO_INCREMENT,
-            `h_name` varchar(55) COLLATE utf8_unicode_ci DEFAULT NULL,
-            `h_result` enum('عالی','خوب','بد') COLLATE utf8_unicode_ci DEFAULT NULL,
-            `h_luckyday` varchar(155) COLLATE utf8_unicode_ci DEFAULT NULL,
-            `h_dobeity` text COLLATE utf8_unicode_ci DEFAULT NULL,
-            `h_description` text COLLATE utf8_unicode_ci DEFAULT NULL,
-            `created_at` date DEFAULT NULL,
-            `updated_at` date DEFAULT NULL,
-            PRIMARY KEY (`id`)
-          ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
-		";
-    dbDelta($createHoroscopesTable);
 
+    register_uninstall_hook(__FILE__, 'prophetsHoroscopeUninstall');
     flush_rewrite_rules();
 }
 
 function MYPH_deactivate_function()
 {
     flush_rewrite_rules();
+}
+
+function prophetsHoroscopeUninstall()
+{
+    //if (get_option('should_delete_myph_db')) {
+    global $wpdb;
+    $table = $wpdb->prefix . MYPH_HOROSCOPES_TBL;
+    $wpdb->query("DROP TABLE IF EXISTS ${table}");
+    //}
 }
---
+++ helpers/db.php
@@ -0,0 +1,22 @@
+<?php defined('ABSPATH') or die('No script kiddies please!');
+
+require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+global $wpdb;
+
+$horoscopes_tbl = $wpdb->prefix . MYPH_HOROSCOPES_TBL;
+$createHoroscopesTable =
+    "
+		CREATE TABLE IF NOT EXISTS `{$horoscopes_tbl}` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `h_name` varchar(55) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+            `h_result` enum('عالی','خوب','بد') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+            `h_luckyday` varchar(155) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+            `h_dobeity` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+            `h_description` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+            `created_at` date DEFAULT NULL,
+            `updated_at` date DEFAULT NULL,
+            PRIMARY KEY (`id`)
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+		";
+
+dbDelta($createHoroscopesTable);
