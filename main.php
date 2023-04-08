011ee6b6b343e42a340bd5be96b07058233632c8
ali <wizard2070@gmail.com>
2023-04-08 22:06:36 +0430

    hide-admin-menu

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
  * Plugin Name: Horoscope of the prophets
  * Plugin URI:  http://localhost
  * Description: Use [insert_horoscope] to view horoscope shortcode on the page.
- * Version: 1.0.4
+ * Version: 1.0.5
  * Author: ZeroOne
  * Author URI: https://github.com/tuderiewsc
  * Text Domain: prophets_horoscope
@@ -15,7 +15,7 @@ define('MYPH_ROOTDIR', plugin_dir_path(__FILE__));
 define('MYPH_INC', MYPH_ROOTDIR . 'includes/');
 define('MYPH_ADMIN', MYPH_ROOTDIR . 'admin/');
 define('MYPH_ADMIN_PAGES', MYPH_ROOTDIR . 'admin/pages/');
-define('MYPH_ADMIN_JS', plugin_dir_url(__FILE__) . 'admin/static/js/');
+define('MYPH_ADMIN_CSS', plugin_dir_url(__FILE__) . 'admin/static/css/');
 define('MYPH_SITE_JS', plugin_dir_url(__FILE__) . 'site/static/js/');
 define('MYPH_SITE_CSS', plugin_dir_url(__FILE__) . 'site/static/css/');
 define('MYPH_SITE_IMAGES', plugin_dir_url(__FILE__) . 'site/static/images/');
@@ -30,6 +30,11 @@ if (!function_exists('get_plugin_data'))
     require_once(ABSPATH . 'wp-admin/includes/plugin.php');
 
 
+add_action('admin_enqueue_scripts', function () {
+    $pluginVersion = (get_plugin_data(__FILE__, false))['Version'];
+
+    wp_enqueue_style('myph-admin-styles', MYPH_ADMIN_CSS . 'admin-styles.css', array(), $pluginVersion);
+});
 add_action('wp_enqueue_scripts', function () {
     $pluginVersion = (get_plugin_data(__FILE__, false))['Version'];
 
@@ -58,4 +63,14 @@ include(MYPH_INC . 'shortcodes.php');
 if (is_admin()) {
     include(MYPH_ADMIN . 'ajax_requests.php');
     include(MYPH_ADMIN . 'admin_process.php');
+
+    /** Add settings link to plugin-title  */
+    add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'myph_settings_link');
+    function myph_settings_link(array $links)
+    {
+        $url = get_admin_url() . "admin.php?page=myph";
+        $settingsLink = '<a href="' . $url . '">' . __('Settings', 'prophets_horoscope') . '</a>';
+        $links[] = $settingsLink;
+        return $links;
+    }
 }
